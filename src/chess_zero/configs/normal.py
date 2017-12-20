0464c31bd1843ae08496f265b2d37fe6e39f80fc
Alexander Lenhardt <alexander.lenhardt@soundcloud.com>
2017-12-21 00:57:12 +0100

    normal config updates

--- src/chess_zero/configs/normal.py
+++ src/chess_zero/configs/normal.py
@@ -5,7 +5,6 @@ class EvaluateConfig:
         self.play_config = PlayConfig()
         self.play_config.simulation_num_per_move = 200
         self.play_config.thinking_loop = 1
-        self.play_config.c_puct = 1
         self.play_config.change_tau_turn = 0
         self.play_config.noise_eps = 0
         self.evaluate_latest_first = True
@@ -15,33 +14,34 @@ class PlayDataConfig:
     def __init__(self):
         self.nb_game_in_file = 100
         self.max_file_num = 200  # 5000
-
+        self.save_policy_of_tau_1 = True
+        
 
 class PlayConfig:
     def __init__(self):
-        self.simulation_num_per_move = 200
+        self.simulation_num_per_move = 400
         self.thinking_loop = 1
         self.logging_thinking = False
-        self.c_puct = 2
+        self.c_puct = 1.5
         self.noise_eps = 0.25
-        self.dirichlet_alpha = 0.03
+        self.dirichlet_alpha = 0.5
         self.change_tau_turn = 10
         self.virtual_loss = 3
         self.prediction_queue_size = 16
         self.parallel_search_num = 16
         self.prediction_worker_sleep_sec = 0.0001
         self.wait_for_expanding_sleep_sec = 0.00001
-        self.resign_threshold = -13
+        self.resign_threshold = -40
         self.min_resign_turn = 5
 
 
 class TrainerConfig:
     def __init__(self):
-        self.batch_size = 32  # 2048
+        self.batch_size = 512  # 2048
         self.epoch_to_checkpoint = 1
         self.start_total_steps = 0
         self.save_model_steps = 2000
-        self.load_data_steps = 1000
+        self.load_data_steps = 2000
 
 
 class ModelConfig:
